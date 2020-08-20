6028198934fe94b4ac80ba27b1673c8bf06fbc00
Jesus Hdez <Jessiehdez36@hotmail.com>
2020-08-20 10:46:59 -0500

    Se creo el fichero docs

--- webpack.prod.js
+++ webpack.prod.js
@@ -62,7 +62,7 @@ module.exports = {
     plugins: [
         new HtmlWebPackPlugin({
             template: './src/index.html',
-            filename: './index.html'
+            filename: '.index.html'
         }),
         new MiniCssExtractPlugin({
             filename: '[name].[contentHash].css',
